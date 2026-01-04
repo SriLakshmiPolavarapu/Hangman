a53dd64d67127d59412b536f68d429036b26563f
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2026-01-03 23:38:05 -0800

    Add code for the hangman stand

--- Hangman/src/HangmanDrawing.tsx
+++ Hangman/src/HangmanDrawing.tsx
@@ -1,4 +1,53 @@
 export function HangmanDrawing()
 {
-    
+    return <div style={
+        {
+            position: "relative"
+        }
+    }>
+        <div style={
+            {
+                height: "50px",
+                width: "10px",
+                background: "blue",
+                position: "absolute",
+                top: 0,
+                right:0,
+            }
+        }>
+        </div>
+
+        <div style={
+            {
+                height: "10px",
+                width: "200px",
+                background: "blue",
+                marginLeft: "120px",
+                
+            }
+        }>
+        </div>
+
+        <div style={
+            {
+                height: "400px",
+                width: "10px",
+                background: "blue",
+                marginLeft: "120px" , //120px because it's the half of width: 250px
+                
+            }
+        }>
+        </div>
+        <div style={
+            {
+                height: "10px",
+                width: "250px",
+                background: "blue"
+            }
+        }>
+
+        </div>
+
+    </div>
+
 }
