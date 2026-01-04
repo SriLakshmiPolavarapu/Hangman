2939b1b9ba241e490176fbdd024dc99488d41e4d
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2026-01-04 14:06:57 -0800

    Add code for the hangman word

--- Hangman/src/HangmanWord.tsx
+++ Hangman/src/HangmanWord.tsx
@@ -1,4 +1,21 @@
 export function HangmanWord()
 {
-    
+    const word = "Hello";
+    return(
+    <div style={
+        {
+            display: "flex",
+            gap: "0.25rem",
+            fontSize: "6rem",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            fontFamily: "monospace"
+        }
+    }>
+
+    {word.split("").map((letter, index) =>
+    <span> {letter} </span>
+    )}    
+    </div>
+    )
 }
